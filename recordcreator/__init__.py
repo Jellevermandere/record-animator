9a6084aaf5ab62eb80bc6d72dfd69627442da0fa
Jelle Vermandere <86951039+JelleKUL@users.noreply.github.com>
2023-08-09 23:36:45 +0200

    vinyl creator

--- recordcreator/__init__.py
+++ recordcreator/__init__.py
@@ -1,1 +1,17 @@
-import os
+import cv2
+import numpy as np
+
+
+def resize_pie():
+    # resizes and pads the image into the desired size
+    pass
+
+def place_pies(pies):
+    # equally distributes the pies parts over a circular image
+
+    img = np.zeros((2048,2048,3), dtype=np.uint8)
+
+    cv2.circle(img, (1024,1024), 1024)
+
+    for pie in pies:
+        pass
